32f72439dede309ce0e2236ae63d398133af4662
Adrian.bozhankov <adrian.bozhankov@gmail.com>
2015-01-09 14:45:14 +0200

    Added home controller and home view

---
+++ app/js/app.js
@@ -0,0 +1,8 @@
+var app = angular.module('adsApp', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'HomeCtrl'
+	});
+}]);
